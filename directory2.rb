09247228cc8615b588b0c0b163ef4750112ac520
Nick Butler <nwmbutler@gmail.com>
2020-05-06 12:12:28 +0100

    directory2 adjusted for single student

--- directory2.rb
+++ directory2.rb
@@ -1,33 +1,38 @@
 def input_students
-  puts "Please enter the name of a student, hit enter twice when you're finished"
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
-  puts "Please enter a hobby"
-  hobby = gets.chomp
   while !name.empty? do
-    students << {name: name, hobby: hobby, cohort: :november}
+    students << {name: name, cohort: :november}
+    if students.count == 1
+    puts "Now we have 1 student"
+    else
     puts "Now we have #{students.count} students"
+    end
     name = gets.chomp
   end
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+def print_header(students)
+  if students.count == 0
+    puts "We currently have no students"
+  else
+  puts "The students of Villians Academy"
+  puts "_____________"
 end
-
+end
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} hobby: #{student[:hobby]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
-print_header
+print_header(students)
 print(students)
 print_footer(students)
