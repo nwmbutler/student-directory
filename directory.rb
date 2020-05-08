6260af5696ace6deab0a1da530ccb8c7e35013c6
Nick Butler <nwmbutler@gmail.com>
2020-05-08 16:05:14 +0100

    Villains saving to file

--- directory.rb
+++ directory.rb
@@ -11,6 +11,16 @@ def input_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   loop do
     print_menu
@@ -21,6 +31,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -36,6 +47,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
